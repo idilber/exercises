9f94238dec7b2ae7a8bcde82d83357355e977a68
Idil <151859466+idilber@users.noreply.github.com>
2023-11-24 13:42:19 +0300

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,8 @@
+while True:
+    number = int(input("Enter a number: "))
+    if number%2==0:
+        print("The number is an even number.")
+    else:
+        print("The number ıs an odd number.")
+
+        
