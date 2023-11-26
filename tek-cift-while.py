8ee62bd77d2fe6bbcc3ec0a4b2ff124303aead4b
Idil <151859466+idilber@users.noreply.github.com>
2023-11-26 16:21:08 +0300

    Add files via upload

---
+++ tek-cift-while.py
@@ -0,0 +1,14 @@
+i = 1
+
+n=int(input("Tavan sayisi: "))
+while i <= n:
+    if i%2!=0:
+        kare = i*i
+        print(i,"in karesi= ",kare)
+    else:
+        kup = i*i*i
+        print(i,"in küpü= ",kup)
+    i=i+1
+
+
+    
