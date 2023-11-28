faa70cabe1b9aef759b1a70f8d9359e10ec6773f
Idil <151859466+idilber@users.noreply.github.com>
2023-11-28 22:58:09 +0300

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,9 @@
+for i in range (7):
+    for j in range(i+1):
+        print("*",end='')
+    print("")
+print("\n")
+for i in range (7,0,-1):
+    for j in range(i,0,-1):
+        print("*",end='')
+    print("")  
