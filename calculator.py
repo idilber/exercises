cfd60689fb802e03ec3f2e17915f75453a95a9bb
Idil <151859466+idilber@users.noreply.github.com>
2023-11-28 11:03:08 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,16 +1,24 @@
-n1=int(input("Enter a number: "))
-n2=int(input("Enter another number: "))
-op=input("Choose your operation: +,-,*,/ ")
+while True:
+    n1=int(input("Enter a number: "))
+    n2=int(input("Enter another number: "))
+    op=input("Choose your operation: +,-,*,/ ")
 
-if op == "+":
-    Result=n1+n2
-elif op == "-":
-    Result=n1-n2
-elif op == "*":
-    Result=n1*n2
-elif op == "/":
-    Result=n1/n2
-else:
-    print("Wrong choice")
-print("Result is: ",Result)
+    if op == "+":
+        Result=n1+n2
+    elif op == "-":
+        Result=n1-n2
+    elif op == "*":
+        Result=n1*n2
+    elif op == "/":
+        Result=n1/n2
+    else:
+        print("Wrong choice")
+    print("Result is: ",Result)
+    
+    answer=input("Do you want to continue? Y/N")
+    if answer == "N":
+        print("Have a good day!")
+        break
+    else:
+        continue
     
